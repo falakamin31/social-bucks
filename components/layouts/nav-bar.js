711dfe9736a982aa37305df488223a4be81a4b0b
Falak Amin <91012681+falakamin31@users.noreply.github.com>
2024-04-15 19:46:51 +0500

    Added navbar for mobile and desktop

---
+++ components/layouts/nav-bar.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const NavBar = props => {
+    return (
+        <header>
+            <nav className='max-w-[120rem] mx-auto'>
+                <div className="h-[2.4375rem] lg:h-[4.8125rem] bg-black flex justify-end items-end px-[2.625rem] relative">
+                    <div className='lg:hidden absolute bottom-0 left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+                        <Image src='/logo.svg' alt='logo' width={110} height={14} className='w-[6.875rem] h-[0.875rem] object-contain' priority/>
+                    </div>
+                    <div className='hidden lg:block absolute bottom-0 left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+                        <Image src='/logo.svg' alt='logo' width={190}height={24} className='w-[11.875rem] h-[1.5rem] object-contain' priority/>
+                    </div>
+                    <Button/>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
+
+const Button = () => {
+    return (
+        <Link href='/'>
+        <button className='hidden lg:block text-right capitalize bg-green px-12 py-3 rounded-[10px] text-[15px] font-eina font-bold hover:bg-dark-green transition-all ease-in-out duration-200'>
+            Start My Page
+        </button>
+        </Link>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-const Home = (props) => {
-    return <>
-        <p className="text-dark-black font-bold">home</p>
-    </>
+import NavBar from "@/components/layouts/nav-bar";
+import { Head } from "next/document";
+
+const Home = props => {
+    return (
+        <>
+            {/* <Head>
+                <title>Social Bucks</title>
+                <meta name='description' content='Social Bucks is the best way for creators and artists to accept support and membership from their fans.'/>
+                <link rel='icon' href='/logo.svg' />
+            </Head> */}
+
+            <NavBar/>
+        </>
+    );
 };
 
 export default Home;
