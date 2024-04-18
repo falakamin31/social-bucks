a266539fb28d56880eb7a96f4e67a23d684eb8c2
Falak Amin <91012681+falakamin31@users.noreply.github.com>
2024-04-18 12:39:23 +0500

    Added cards for why-social-bucks section for larger devices

--- components/home/carousel/carousel.js
+++ components/home/carousel/carousel.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import { Scrollbar } from 'swiper/modules';
-import 'swiper/css/scrollbar';
-
-import { carouselCards } from '@/constants';
 import Image from 'next/image';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Scrollbar } from 'swiper/modules';
+import 'swiper/css/scrollbar';
+import 'swiper/css';
 
-export default function App() {
+import { carouselCards } from '@/constants';
+
+export default function Carousel() {
 
   return (
+    <>
     <div className='md:hidden'>
       <Swiper
         scrollbar={{ hide: true }}
         slidesPerView={1.7}
         spaceBetween={20}
-        // centeredSlides={true}
         grabCursor
         simulateTouch
         className="!pl-9 !pr-9 !py-7 h-full"
@@ -28,20 +27,22 @@ export default function App() {
             </SwiperSlide>
         ))}
       </Swiper>
-
     </div>
+    <div className='hidden md:flex gap-4 2xl:gap-[25px] mt-[2.875rem] px-4'>
+            {carouselCards.map((card) => <li className='list-none'><Card card={card} /></li>)}
+      </div>
+      </>
   );
 }
 
 const Card = ({card}) => {
     const {title, description, image} = card;
-    console.log(title, description, image)
     return (
-        <div className=" bg-dark-gray h-[14rem] rounded-[13px] border-[0.7px] border-white flex flex-col gap-[2.4375rem] lg:gap-[4.3125rem] py-[2.0625rem] lg:py-[3.625rem] px-[0.9375rem] lg:px-[1.3125rem]">
-        <Image src={image} alt={title} width={20} height={20} className="w-5 h-5 lg:w-[30px] lg:h-[30px] self-end" />
+        <div className="md:max-w-[19.1875rem] bg-dark-gray h-[14rem] xl:h-[19rem] rounded-[13px] border-[0.5px] border-white flex flex-col gap-[2.4375rem] md:gap-[1.5rem] xl:gap-[2.3rem] 2xl:gap-[4.3125rem] py-[2.0625rem] md:py-[1.3rem] px-[0.9375rem] hover:border-[#72BC0B] transition-all ease-linear hover:cursor-pointer">
+        <Image src={image} alt={title} width={20} height={20} className="w-5 h-5 xl:w-[30px] xl:h-[30px] self-end" />
         <div className='text-left'>
-        <h2 className="text-white font-bold text-xs lg:text-lg">{title}</h2>
-        <p className="mt-[1.5px] font-medium text-[10px] lg:text-[15px] text-white">{description}</p>
+        <h2 className="text-white font-bold text-xs xl:text-lg">{title}</h2>
+        <p className="mt-[6px] 2xl:mt-[1rem] font-medium text-[10px] xl:text-[15px] text-white">{description}</p>
         </div>
     </div>
     );
