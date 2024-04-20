ad61be70bd6323d5f77e2272a6772bc0c81d52a0
Falak Amin <91012681+falakamin31@users.noreply.github.com>
2024-04-21 01:34:06 +0500

    Removed icons in hero section for mobile

--- components/home/hero/cards.js
+++ components/home/hero/cards.js
@@ -17,7 +17,7 @@ const Cards = props => {
                     </div>
                         </div>
                         {/* Gift Icon  */}
-                        <div className="absolute bg-white px-2 py-2 w-[3.5rem] h-[3.5rem] border-[0.5px] rounded-full z-40">
+                        <div className="hidden lg:block absolute bg-white px-2 py-2 w-[3.5rem] h-[3.5rem] border-[0.5px] rounded-full z-40">
                             <span className="text-4xl">🎁</span>
                         </div>
                     </div>
@@ -27,7 +27,7 @@ const Cards = props => {
                 <div className="max-w-[10.1875rem] lg:max-w-[20.25rem] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <div className="relative left-[17%] -top-[45px] xl:top-[20px] xl:-left-[85%] ">
                         {/* Diamond Icon  */}
-                        <div className="absolute bg-white px-2 py-2 w-[3.5rem] h-[3.5rem] border-[0.5px] rounded-full z-40 -top-[31%] left-[0%]">
+                        <div className="hidden lg:block absolute bg-white px-2 py-2 w-[3.5rem] h-[3.5rem] border-[0.5px] rounded-full z-40 -top-[31%] left-[0%]">
                             <span className="inline-block h-[2.0625rem] w-[2.0625rem] text-4xl">💎</span>
                         </div>
                         <div className="bg-white pt-[1.2rem] lg:pt-[2.6875rem] max-w-[10.1875rem] md:max-w-[20.25rem] rounded-xl flex flex-col gap-2 lg:gap-7 drop-shadow-2xl  rotate-6 pb-3 z-[200]">
@@ -82,7 +82,7 @@ const Cards = props => {
                 <div className="max-w-[10.1875rem] lg:max-w-[20.25rem] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
                     <div className="relative left-[45%] xl:left-[180%] top-[110px] xl:top-[10px]">
                          {/* Beat Icon  */}
-                        <div className="absolute bg-white px-2 py-2 w-[3.5rem] h-[3.5rem] border-[0.5px] rounded-full z-40 -right-[13%] top-[30%]">
+                        <div className="hidden lg:block absolute bg-white px-2 py-2 w-[3.5rem] h-[3.5rem] border-[0.5px] rounded-full z-40 -right-[13%] top-[30%]">
                             <span className="text-4xl">🧸</span>
                         </div>
                         <div className="bg-white pt-[1.2rem] lg:pt-[2.6875rem] max-w-[10.1875rem] md:max-w-[20.25rem] rounded-xl flex flex-col gap-2 lg:gap-7 drop-shadow-2xl -rotate-5 pb-3 z-[200]">
